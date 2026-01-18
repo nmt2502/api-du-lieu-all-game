0a1c2e93722d9f4d0c798b55062a5d6e6da41245
nmt2502 <anhlatuan2502@gmail.com>
2026-01-18 14:30:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ const SUNWIN_PATTERNS = {
   ],
 
   "4-1-5-1-1": [
-    { pattern: ["T","T","T","T","X","T","T","T","T","T"."X","T"], probability: 0.87, strength: 0.77 },
+    { pattern: ["T","T","T","T","X","T","T","T","T","T","X","T"], probability: 0.87, strength: 0.77 },
     { pattern: ["X","X","X","X","T","X"."X","X","X","X"."T","X"], probability: 0.87, strength: 0.77 }
   ],
 
